4116c5e800965f32c101df297005345d56a1b66b
Suxrob <salomsuxrob@gmail.com>
2022-11-03 18:56:48 +0500

    added new feature

--- post/urls.py
+++ post/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 
  path('', views.post_list, name='post_list'),
  path('post/<slug:slug>', views.post_detail, name='post_det'),
- path('tag/<slug:tag_slug>', views.post_detail, name='post_tag'),
+ path('tag/<slug:tag_slug>', views.post_list, name='post_tag'),
 ]
