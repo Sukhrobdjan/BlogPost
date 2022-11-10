f05fb569475da203ff1211fb6d079c4845655269
Suxrob <salomsuxrob@gmail.com>
2022-11-10 17:02:44 +0500

    fix:some changed

--- core/settings.py
+++ core/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
